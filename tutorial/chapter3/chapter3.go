671591aee53eadea1a135cf791b381db69814a8b
lullasy <cid378@gmail.com>
2017-07-03 22:55:52 +0900

    p.57 - fmt.Printf便利 - 宣言に型明示的に指定できるのはいいけど、末尾に書くから見づらそう？

---
+++ tutorial/chapter3/chapter3.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 通常の宣言 型指定
+	var x, y int
+	// 代入 便利！
+	x, y = 1, 2
+	fmt.Printf("%d, %d\n", x, y)
+
+	// デバッグに便利
+	fmt.Printf("データ型埋め込み %v, リテラル表現 %#v, 型情報 %T\n", [...]int{1, 2, 3}, [...]int{1, 2, 3}, [...]int{1, 2, 3})
+}
