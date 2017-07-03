1ab324d6d509cb7d1b98c3bd8b379b69f8af7ca6
lullasy <cid378@gmail.com>
2017-07-03 23:14:37 +0900

    p.62 - パッケージ変数 - つかってない変数はダメー

--- tutorial/chapter3/chapter3.go
+++ tutorial/chapter3/chapter3.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+// パッケージ変数実質グローバル
+var pkg = 100
+
 func main() {
 	// 通常の宣言 型指定
 	var x, y int
@@ -15,6 +18,14 @@ func main() {
 	// 宣言と代入一緒にする、返り値が変数の型になる
 	n := one()
 	fmt.Println(n)
+
+	pkg = pkg + 1
+	fmt.Println(pkg)
+
+	a := 1
+	// b := 2 error
+	// c := 3 error
+	fmt.Println(a)
 }
 
 func one() int {
